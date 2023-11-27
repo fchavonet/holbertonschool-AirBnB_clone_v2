352ce7b922b4b1b1fb5777505ff956e5d3e14ad6
fchavonet <fchavonet@mailo.com>
2023-11-27 17:56:41 +0100

    Update the `def do_create(self, arg)` method.

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,15 +115,45 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+
+        # Check if the class name is provided
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        # Split the arguments into a list
+        args_list = args.split(" ")
+
+        # Check if the specified class exists
+        if args_list[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        # Create a new instance of the specified class
+        new_instance = HBNBCommand.classes[args_list[0]]()
+
+        # Process each argument after the class name
+        for arg in args_list[1:]:
+            # Split the argument into key and value
+            key, value = arg.split("=")
+            # Replace underscores with spaces in the value
+            value = value.replace("_", " ")
+
+            # Check the type of value and set the attribute accordingly
+            if value[0] == "\"":
+                # If the value is enclosed in double quotes, remove them
+                setattr(new_instance, key, value[1:-1])
+            elif "." in value:
+                # If the value contains a dot, convert it to a float
+                setattr(new_instance, key, float(value))
+            else:
+                # Otherwise, convert the value to an integer
+                setattr(new_instance, key, int(value))
+
+        # Print the ID of the newly created instance
         print(new_instance.id)
+        # Save the new instance and update the storage
+        new_instance.save()
         storage.save()
 
     def help_create(self):
@@ -187,7 +217,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +302,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +310,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +349,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
