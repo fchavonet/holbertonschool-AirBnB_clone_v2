e1244627888ad885080c9196220f1e6d48b1b122
fchavonet <fchavonet@mailo.com>
2023-11-28 14:45:14 +0100

    Update `DBStorage` class with `reload` method.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Define DBStorage engine"""
 
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import create_engine
-from os import getenv
+from models.base_model import BaseModel, Base
+
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 class DBStorage:
     """Private class attributes"""
@@ -21,7 +23,7 @@ __session = None
 def __init__(self):
     self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
                                   .format(getenv("HBNB_MYSQL_USER"),
-                                    getenv("HNBN_MYSQL_PWD"),
+                                    getenv("HBNB_MYSQL_PWD"),
                                     getenv("HBNB_MYSQL_HOST"),
                                     getenv("HBNB_MYSQL_DB")),
                                     pool_pre_ping=True)
@@ -56,3 +58,13 @@ def delete(self, obj=None):
     """delete from the current database session"""
     if obj is not None:
         self.__session.delete(obj)
+
+def reload(self):
+    """Create all tables and initialize the session"""
+
+    from models import base_model
+
+    Base.metadata.create_all(self.__engine)
+
+    Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+    self.__session = scoped_session(Session)
