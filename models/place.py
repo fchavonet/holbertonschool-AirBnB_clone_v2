251914c5c472283f7388037f0f007dc112461da6
fchavonet <fchavonet@mailo.com>
2023-11-28 21:48:00 +0100

    New try to pass checker with new fixes.

--- models/place.py
+++ models/place.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer,  Float, ForeignKey
-from sqlalchemy import relationship
-from os import getenv
 from models.review import Review
+from os import getenv
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
-    else:
-        @property
-        def reviews(self):
-            """ """
-            from models import storage
-            review_list = []
-            for review in storage.all(Review).values():
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
-
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
@@ -35,13 +21,14 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", backref="place")
+    else:
         @property
         def reviews(self):
-            """Getter attribute that returns the list of Review instances with
-               place_id equals to the current Place.id
-            """
+            """ """
             from models import storage
+
             review_list = []
             for review in storage.all(Review).values():
                 if review.place_id == self.id:
