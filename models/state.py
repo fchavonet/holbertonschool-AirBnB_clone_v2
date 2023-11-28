248f26ce8ad269e3707ad4076614da69d7072caf
Nicolaslegoat <n.mancino03@gmail.com>
2023-11-28 02:08:13 -0800

    update city and state file

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
