a5276a9a16b4adc02ee77dee31efd001aeb87f8d
fchavonet <fchavonet@mailo.com>
2023-11-28 11:46:31 +0100

    Update State Class and __init__.

--- models/state.py
+++ models/state.py
@@ -5,10 +5,20 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_instances = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_instances.append(city)
+            return city_instances
